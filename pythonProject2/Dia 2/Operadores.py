147239166a28ba56ea47442e2a7b63ac258eba67
henryagm <henrygonzalezm.hg@gmail.com>
2024-03-23 16:27:48 -0300

    actuali

--- pythonProject2/Dia 2/Operadores.py
+++ pythonProject2/Dia 2/Operadores.py
@@ -8,3 +8,6 @@ print(f"{x} mas {y} es igual a {x/y}")
 print(f"{z} dividido al piso de {y} es igual a {z//y}")
 print(f"{z} modulo de {y} es igual a {z%y}")
 print(f"{x} elevado a {y} es igual a {x**y}")
+print(f"{x} elevado a {y} es igual a {x**3}")
+print(f"la raiz cuadrada de {x} es {x**0.5}")
+
---
+++ pythonProject2/Dia 2/Redonderar.py
@@ -0,0 +1,8 @@
+#resultado = round(98/7)
+#print(resultado)
+
+valor = round(95.66666666)
+
+print(valor)
+
+print(type(valor))
