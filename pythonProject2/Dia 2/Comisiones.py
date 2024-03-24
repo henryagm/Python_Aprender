4e0cea95b4644dd3e478223cdcbfa7f252f357ea
henryagm <henrygonzalezm.hg@gmail.com>
2024-03-24 15:54:13 -0300

    create comisiones

---
+++ pythonProject2/Dia 2/Comisiones.py
@@ -0,0 +1,6 @@
+nombre = input("por fsvor dime tu nombre: ")
+ventas = int(input("Diga sus ventas totales del mes: "))
+
+comisiones = round(ventas * 13 / 100,2)
+
+print(f"Hola {nombre}, tus comisiones de este mes son de {comisiones}")
