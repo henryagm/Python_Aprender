c9e5044fb0074b173f3ae6e8415cca97c4843dbf
henryagm <henrygonzalezm.hg@gmail.com>
2024-03-25 00:08:03 -0300

    iji
    jin

--- pythonProject2/Dia 2/Comisiones.py
+++ pythonProject2/Dia 2/Comisiones.py
@@ -1,6 +1,10 @@
+#Primer ejercicio en python
 nombre = input("por fsvor dime tu nombre: ")
 ventas = int(input("Diga sus ventas totales del mes: "))
 
-comisiones = round(ventas * 13 / 100,2)
 
+comisiones = round(ventas * 13 / 100,2)
 print(f"Hola {nombre}, tus comisiones de este mes son de {comisiones}")
+
+
+
---
+++ pythonProject2/Dia 3/index.py
@@ -0,0 +1,7 @@
+mi_texto="Esta es una prueba"
+resultado = mi_texto.index("prueba")
+
+resultado = mi_texto.rindex("a")
+print(resultado)
+
+
