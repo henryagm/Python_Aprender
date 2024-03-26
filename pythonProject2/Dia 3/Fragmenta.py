d68dc4aebaaf4f5f504a82b50b065802afb9da3f
henryagm <henrygonzalezm.hg@gmail.com>
2024-03-25 21:40:05 -0300

    varios}_metodos
    aprender varios metodos

---
+++ pythonProject2/Dia 3/Fragmenta.py
@@ -0,0 +1,5 @@
+texto = "ABCDEFGHI"
+fragmento = texto[2:]
+#se empieza desde el segundo caracter hasta el final
+#colocando ::-1 se invieete toda la cadena de texto
+print(fragmento)
---
+++ pythonProject2/Dia 3/Metodos.py
@@ -0,0 +1,11 @@
+texto = "este es el texto de henry"
+
+resultado = texto.split()
+
+#el metodo split es para hacer una lista, el upper para poner
+#en mayusculas
+# el metodo lower en colocar
+#todo en minusculas
+
+
+print(resultado)
