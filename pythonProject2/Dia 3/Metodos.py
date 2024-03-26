32f8016686b121acc1570d88345852a514cad2c6
henryagm <henrygonzalezm.hg@gmail.com>
2024-03-26 20:55:46 -0300

    ekjfehk
    kjhkj

--- pythonProject2/Dia 3/Metodos.py
+++ pythonProject2/Dia 3/Metodos.py
@@ -1,11 +1,29 @@
 texto = "este es el texto de henry"
 
-resultado = texto.split()
+#resultado = texto.split()
 
-#el metodo split es para hacer una lista, el upper para poner
-#en mayusculas
+#el metodo split es para hacer una lista,
+# el upper para poner en mayusculas
 # el metodo lower en colocar
+# el metodo join es para convertir de lista a cadena de texto normal
+
 #todo en minusculas
 
 
+#print(resultado)
+
+a = "aprender"
+b = "Python"
+c = "es"
+d = "genial"
+e = "-". join([a,b,c,d])
+
+#print(e)
+
+# si yo coloco find como metodo puedo buscar un caractaer
+resultado = texto.replace("x","66")
 print(resultado)
+
+
+
+
--- pythonProject2/Dia 3/index.py
+++ pythonProject2/Dia 3/index.py
@@ -3,11 +3,9 @@ resultado = mi_texto.index("prueba")
 
 resultado = mi_texto.rindex("a")
 print(resultado)
+print("hola mundo")
 
-
-
-
-
-
+while 1<2 or 3>4:
+    print("1 es menor a 2, o 3 es mayor a 4")
 
 
